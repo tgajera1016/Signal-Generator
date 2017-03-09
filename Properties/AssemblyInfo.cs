66897db4726c88c01bd7698242972f913813eae0
Tejas Gajera <tgajera1016@users.noreply.github.com>
2017-03-09 16:55:41 -0500

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Signal Generator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("KaVo Kerr Group")]
+[assembly: AssemblyProduct("Signal Generator")]
+[assembly: AssemblyCopyright("Copyright © KaVo Kerr Group 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7cb51b0c-0e75-4db1-b195-ea555d61cd6a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SignalGenerator.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Signal_Generator
+{
+    /// <summary>
+    /// delegate for new signal data generated
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="data"></param>
+    public delegate void OnDataGenerated(object sender, Queue<double> data);
+
+    /// <summary>
+    /// signal class which holds signal properties
+    /// </summary>
+    public class SignalGenerator
+    {
+        /// <summary>
+        /// constructor to set signal properties
+        /// </summary>
+        /// <param name="phase"></param>
+        /// <param name="amplitude"></param>
+        /// <param name="frequency"></param>
+        /// <param name="sampleFrequency"></param>
+        /// <param name="time"></param>
+        public SignalGenerator(double phase, double amplitude, double frequency, double sampleFrequency, double time)
+        {
+            _time = time;
+            _phase = phase;
+            _amplitude = amplitude;
+            _frequency = frequency;
+            _sampleFrequency = sampleFrequency;
+
+            _interval = 1.0 / SampleFrequency;
+            _timeStamp = 0.0;
+
+            UpdateSignalDataQueue();
+
+            _busy.Set();
+        }
+
+        /// <summary>
+        /// copy constructor
+        /// </summary>
+        /// <param name="source"></param>
+        public SignalGenerator(SignalGenerator source)
+        {
+            if (source == null) return;
+            if (this == source) return;
+
+            Time = source.Time;
+            Phase = source.Phase;
+            Amplitude = source.Amplitude;
+            Frequency = source.Frequency;
+            SampleFrequency = source.SampleFrequency;
+        }
+
+        /// <summary>
+        /// start to generate signal data
+        /// </summary>
+        public void Start()
+        {
+            if (_signalData == null)
+                return;
+           
+            _signalData.Clear();
+
+            while (_busy.WaitOne())
+            {
+                _dataAvilablEvent.Reset();
+
+                if (_timeStamp > Time) _timeStamp = 0;
+
+                var data = Amplitude * Math.Cos(2 * Math.PI * Frequency * _timeStamp + Phase);
+
+                if (_signalData.Count == _signalLength)
+                    _signalData.Dequeue();
+
+                _signalData.Enqueue(data);
+
+                OnDataGenerated?.Invoke(this, _signalData);
+
+                _timeStamp += _interval;
+
+                Thread.Sleep(10);
+
+                _dataAvilablEvent.Set();
+            }
+        }
+
+        /// <summary>
+        /// stop timer (do not generate signal data)
+        /// </summary>
+        public void Stop()
+        {
+            _dataAvilablEvent.WaitOne();
+            _busy.Reset();
+        }
+
+        /// <summary>
+        /// restart to generate signal data 
+        /// </summary>
+        private void Restart()
+        {
+            _signalData.Clear();
+
+            _interval = 1.0 / SampleFrequency;
+            _timeStamp = 0.0;
+
+            _busy.Set();
+        }
+
+        /// <summary>
+        /// modify size of a signal data queue
+        /// </summary>
+        private void UpdateSignalDataQueue()
+        {
+            _signalLength = (int)Math.Floor(Time * SampleFrequency);
+            _signalData = new Queue<double>(_signalLength);
+        }
+
+        /// <summary>
+        /// length of a signal
+        /// </summary>
+        private int _signalLength;
+
+        /// <summary>
+        /// signal data
+        /// </summary>
+        private Queue<double> _signalData;
+
+        /// <summary>
+        /// time to hold signal data
+        /// </summary>
+        private double _time;
+
+        /// <summary>
+        /// time property
+        /// </summary>
+        public double Time
+        {
+            get {return _time;}
+            set
+            {
+                Stop();
+                _time = value;
+                UpdateSignalDataQueue();
+                Restart();
+            }
+        }
+
+        /// <summary>
+        /// sample frequency
+        /// </summary>
+        private double _sampleFrequency;
+
+        /// <summary>
+        /// sample frequency property
+        /// </summary>
+        public double SampleFrequency
+        {
+            get { return _sampleFrequency; }
+            set
+            {
+                Stop();
+                _sampleFrequency = value;
+                UpdateSignalDataQueue();
+                Restart();
+            }
+        }
+
+        /// <summary>
+        /// phase
+        /// </summary>
+        private double _phase;
+
+        /// <summary>
+        /// phase property
+        /// </summary>
+        public double Phase
+        {
+            get {return _phase;}
+            set
+            {
+                Stop();
+                _phase = value;
+                Restart();
+            }
+        }
+
+        /// <summary>
+        /// amplitude
+        /// </summary>
+        private double _amplitude;
+
+        /// <summary>
+        /// amplitude property
+        /// </summary>
+        public double Amplitude
+        {
+            get { return _amplitude; }
+            set
+            {
+                Stop();
+                _amplitude = value;
+                Restart();
+            }
+        }
+
+        /// <summary>
+        /// frequency
+        /// </summary>
+        private double _frequency;
+
+        /// <summary>
+        /// frequency property
+        /// </summary>
+        public double Frequency
+        {
+            get { return _frequency; }
+            set
+            {
+                Stop();
+                _frequency = value;
+                Restart();
+            }
+        }
+
+        /// <summary>
+        /// time interval between two samples of a signal
+        /// </summary>
+        private double _interval;
+
+        /// <summary>
+        /// amount of time in seconds to record signal data
+        /// </summary>
+        private double _timeStamp;
+
+        /// <summary>
+        /// signal data generated event
+        /// </summary>
+        public event OnDataGenerated OnDataGenerated;
+
+        /// <summary>
+        /// to read serial port data one by one
+        /// </summary>
+        private readonly ManualResetEvent _busy = new ManualResetEvent(false);
+
+        /// <summary>
+        /// auto reset evetnt for thread syncronization
+        /// </summary>
+        private readonly AutoResetEvent _dataAvilablEvent = new AutoResetEvent(false);
+    }
+}
